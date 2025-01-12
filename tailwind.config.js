e1fb3feccbc23d6745934f6a43e0cdb44586902f
aya-soghayyer <128791822+aya-soghayyer@users.noreply.github.com>
2025-01-12 22:09:07 +0200

    create docs folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./docs/**/*.{html,js}'],
   theme: {
     fontFamily:{
       Inter: ['Inter' , 'sans-serif'],
